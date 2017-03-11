39ee581a4313bb07b195ba86040cade536c09554
Patricia Figueroa <patricia.figueroa@ucdenver.edu>
2017-03-11 10:52:56 -0700

    Create app.js

---
+++ app.js
@@ -0,0 +1,26 @@
+//using swagger
+
+'use strict';
+
+var SwaggerExpress = require('swagger-express-mw');
+var app = require('express')();
+module.exports = app;                             // used for testing
+
+var config = {
+  appRoot: __dirname // required config
+};
+
+SwaggerExpress.create(config, function(err, swaggerExpress) {
+  if (err) { throw err; }
+                                                //middleware
+  swaggerExpress.register(app);
+                                               //makes the server be able to accept a parameter from the environment what port to listen on
+  var port = process.env.PORT || 30000;
+  app.listen(port);
+
+  if (swaggerExpress.runner.swagger.paths['/github']) {
+    console.log('try this:\ncurl http://localhost:' + port + '/github');
+  }
+});
+
+
